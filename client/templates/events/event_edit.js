1c9b1a576bce326d1ed0a2592fbb63a523e98053
Michael Klopf <klopf.michael@gmail.com>
2015-01-27 23:16:12 +0100

    Adding validation of event submit and edit.

--- client/templates/events/event_edit.js
+++ client/templates/events/event_edit.js
@@ -1,17 +1,39 @@
+Template.eventEdit.created = function() {
+  Session.set('eventEditErrors', {});
+}
+
+Template.eventEdit.helpers({
+  errorMessage: function(field) {
+    return Session.get('eventEditErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('eventEditErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.eventEdit.events({
   'submit form': function(e) {
     e.preventDefault();
 
     var currentEventId = this._id;
 
-    var date = moment($(e.target).find('[name=eventDate]').val(), 'DD.MM.YYYY hh:mm')._d;
-
     var eventProperties = {
       title: $(e.target).find('[name=title]').val(),
+      eventDate: $(e.target).find('[name=eventDate]').val()
+    }
+
+    var errors = validateEvent(eventProperties);
+    if (errors.title || errors.eventDate)
+      return Session.set('eventEditErrors', errors);
+
+    var date = moment(eventProperties.eventDate, 'DD.MM.YYYY hh:mm')._d;
+
+    var event = {
+      title: eventProperties.title,
       eventDate: new Date(date)
     }
 
-    Events.update(currentEventId, {$set: eventProperties}, function(error) {
+    Events.update(currentEventId, {$set: event}, function(error) {
       if (error) {
         // display the error to the user
         throwError(error.reason);
--- client/templates/events/event_submit.js
+++ client/templates/events/event_submit.js
@@ -1,11 +1,39 @@
+Template.eventSubmit.created = function() {
+  Session.set('eventSubmitErrors', {});
+}
+
+Template.eventSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('eventSubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('eventSubmitErrors')[field] ? 'has-error' : '';
+  },
+  dateNow: function() {
+    return moment().format('DD.MM.YYYY hh:mm');
+  }
+});
+
 Template.eventSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
 
-    var date = moment($(e.target).find('[name=eventDate]').val(), 'DD.MM.YYYY hh:mm')._d;
+    var eventProperties = {
+      title: $(e.target).find('[name=title]').val(),
+      eventDate: $(e.target).find('[name=eventDate]').val()
+    }
+
+    if (eventProperties.eventDate === "")
+      eventProperties.eventDate = moment().format('DD.MM.YYYY hh:mm');
+
+    var errors = validateEvent(eventProperties);
+    if (errors.title || errors.eventDate)
+      return Session.set('eventSubmitErrors', errors);
+
+    var date = moment(eventProperties.eventDate, 'DD.MM.YYYY hh:mm')._d;
 
     var event = {
-      title: $(e.target).find('[name=title]').val(),
+      title: eventProperties.title,
       eventDate: new Date(date)
     };
 
