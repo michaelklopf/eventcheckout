e1e11206966bbab6a53409451aedd731108efdef
Michael Klopf <klopf.michael@gmail.com>
2015-04-11 20:39:49 +0200

    Completing new fixture. Starting to incorporate fees.

--- server/fixtures.js
+++ server/fixtures.js
@@ -6,7 +6,7 @@ var date4 = new Date("2015-12-18T12:00:00Z");
 if (Events.find().count() === 0) {
   var now = new Date().getTime();
 
-  // create two users
+  // create a couple new users
   var mkId = Meteor.users.insert({
     profile: { name: 'michael' }
   });
@@ -17,10 +17,28 @@ if (Events.find().count() === 0) {
   });
   var julia = Meteor.users.findOne(jkId);
 
+  var kkId = Meteor.users.insert({
+    profile: { name: 'klaus' }
+  });
+  var klaus = Meteor.users.findOne(kkId);
+
+  var tId = Meteor.users.insert({
+    profile: { name: 'thore' }
+  });
+  var thore = Meteor.users.findOne(tId);
+
   var ev1id = Events.insert({
     title: 'Basar "Die Arche" Herbst 2014',
-    userId: mkId._id,
-    author: mkId.profile.name,
+    userId: mkId,
+    admins: [
+      { id: mkId },
+      { id: jkId }
+    ],
+    cashiers: [
+      { id: tId },
+      { id: kkId }
+    ],
+    author: michael.profile.name,
     feeInPercent: 0.20,
     //eventDate: 'Sun Sep 28 2014 11:00:00 GMT+0200 (CEST)'
     eventDate: date1
@@ -28,8 +46,8 @@ if (Events.find().count() === 0) {
 
   var ev2id = Events.insert({
     title: 'Basar "Die Arche" Frühjahr 2015',
-    userId: mkId._id,
-    author: mkId.profile.name,
+    userId: mkId,
+    author: michael.profile.name,
     feeInPercent: 0.20,
     //eventDate: 'Sun Mar 29 2015 11:00:00 GMT+0200 (CEST)'
     eventDate: date2
@@ -37,8 +55,8 @@ if (Events.find().count() === 0) {
 
   var ev3id = Events.insert({
     title: 'Flohmarkt BRK Rosenmesse 2015',
-    userId: jkId._id,
-    author: jkId.profile.name,
+    userId: jkId,
+    author: julia.profile.name,
     feeInPercent: 0.20,
     //eventDate: 'Sun Jun 21 2015 10:00:00 GMT+0200 (CEST)'
     eventDate: date3
@@ -46,8 +64,8 @@ if (Events.find().count() === 0) {
 
   var ev4id = Events.insert({
     title: 'Flohmarkt Weihnachten',
-    userId: jkId._id,
-    author: jkId.profile.name,
+    userId: jkId,
+    author: julia.profile.name,
     feeInPercent: 0.20,
     //eventDate: 'Sun Jun 21 2015 10:00:00 GMT+0200 (CEST)'
     eventDate: date4
@@ -59,22 +77,22 @@ if (Events.find().count() === 0) {
     userId: michael._id,
     author: michael.profile.name,
     submitted: new Date(now - 5 * 3600 * 1000),
-    list_items: {
-      0: {
-         price: '1',
-         unit: 'euro',
-         description: 'Stuffed teddy bear',
-         customerId: '',
-         cashierId: ''
+    list_items: [
+      {
+        price: '1',
+        unit: 'euro',
+        description: 'Stuffed teddy bear',
+        customerId: '',
+        cashierId: ''
       },
-      1: {
-         price: '2.5',
-         unit: 'euro',
-         description: 'Sweatshirt',
-         customerId: '',
-         cashierId: ''
+      {
+        price: '2.5',
+        unit: 'euro',
+        description: 'Sweatshirt',
+        customerId: '',
+        cashierId: ''
       }
-    }
+    ]
   });
 
   Lists.insert({
