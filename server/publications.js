3287d8aa188cc9e5649be4fbaee5614730f7d99f
Michael Klopf <klopf.michael@gmail.com>
2015-05-01 23:01:09 +0200

    Fixing errors from merge.

--- server/publications.js
+++ server/publications.js
@@ -2,8 +2,6 @@ Meteor.publish('events', function(author) {
   return Events.find();
 });
 
-/*
 Meteor.publish("users", function () {
     return Meteor.users.find({}, {fields: {'username': 1}});
 });
-*/
